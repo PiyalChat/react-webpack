a7000cefee24bcc68a2162bc9a770f6e67265b38
PiyalChat <piyalchatterjee8@gmail.com>
2024-08-30 19:45:31 +0530

    base configs and styles are added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import "./styles.css";
+import IMAGE from "./react.png";
+import VITE from './vite.svg'
+
 type Props = {};
 
 export const App = (props: Props) => {
-  return <div>App</div>;
+  return (
+    <>
+      <h2>React webpack learnings</h2>
+      <img src={IMAGE} alt="react-logo"/>
+      <img src={VITE} alt="vite-logo"/>
+    </>
+  );
 };
